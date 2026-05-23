ad50f4c23e68a7f41b8a6db8d08923feb5f4b1d4
elliotmiles <elliotmiles19@outlook.com>
2026-05-23 14:01:29 +0100

    Add config files

---
+++ lua/conf/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter").setup({
+      ensure_installed = {
+        "c",
+        "cpp",
+        "python",
+        "html",
+        "css",
+        "matlab",
+        "javascript",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query"
+      },
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    })
+  end
+}
