4994e45be006ad968cb1a5b0a71d25326a06d18e
elliotmiles <elliotmiles19@outlook.com>
2026-05-23 14:39:22 +0100

    Fix telescope bug

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
-require "conf"
+-- nvim 0.12 compatibility shim
+if not vim.filetype.ft_to_lang then
+  vim.filetype.ft_to_lang = function(ft)
+    return vim.treesitter.language.get_lang(ft) or ft
+  end
+end
 
+require "conf"
